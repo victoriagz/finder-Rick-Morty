be123627f27be9b3fe3544571798322db4c8c86f
victoriagz <victoriagabrielagonzalez123@hotmail.com>
2024-03-27 08:54:10 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import "../styles/App.scss"
+
+function App (){
+
+  return (
+<h1>hola</h1>
+  );
+}
+
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+// Fichero src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx";
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
