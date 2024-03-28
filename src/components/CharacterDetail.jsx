6daecba7caac75c40462e4551fe0e4ec0801a10b
victoriagz <victoriagabrielagonzalez123@hotmail.com>
2024-03-28 23:04:18 +0100

    styles added

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,23 +1,30 @@
 import { Link } from "react-router-dom";
 import "../styles/Detail.scss";
 import ExitGif from "../images/portal.gif";
+import "animate.css";
 
 function CharacterDetail({ characterDetail }) {
   return (
     <>
-      <div className="character-detail-container">
+      <div className="character-detail-container animate__animated animate__zoomIn">
         <div className="character-detail">
           <img src={characterDetail.picture} alt={characterDetail.name} />
-          <h1>{characterDetail.name}</h1>
-          <p>Specie: {characterDetail.specie}</p>
-          <p>Status: {characterDetail.status}</p>
-          <p>Origin: {characterDetail.origin}</p>
-          <p>Episodes: {characterDetail.episodes}</p>
+          <div className="container2">
+            <div className="title-container">
+              <h1>{characterDetail.name}</h1>
+            </div>
+            <div className="data-container ">
+              <p>SPECIE: {characterDetail.specie}</p>
+              <p>STATUS: {characterDetail.status}</p>
+              <p>ORIGIN: {characterDetail.origin}</p>
+              <p>EPISODES: {characterDetail.episodes}</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="exit-btn-img">
         <img className="exit" src={ExitGif} alt="" />
-        <Link className="btn-link" to="/">
+        <Link className="btn-link animate__animated animate__bounce" to="/">
           Back to home
         </Link>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import "../styles/Header.scss";
 import Pickle from "./Pickle";
+import "animate.css";
 
 function Header() {
   return (
     <div className="header-container">
       <Pickle />
       <img
-        className="header"
+        className="header animate__animated animate__fadeInRightBig"
         src="https://1000marcas.net/wp-content/uploads/2022/04/Rick-and-Morty.png"
         alt="Rick y Morty Logo"
       />
