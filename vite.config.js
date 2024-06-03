e494ac4229ec8ca6121d7511fd0a21a4da0b1dc4
victoriagz <victoriagabrielagonzalez123@hotmail.com>
2024-06-03 11:38:10 +0200

    page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/modulo-3-evaluacion-final-victoriagz/",
+  base: "/finder-Rick-Morty/",
 });
